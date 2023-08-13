ef4236a26fd5645a4d0efe9f7617bcc796098e70
Awais Aslam <awaisaslam336@gmail.com>
2023-08-13 17:15:47 +0500

    chat module design

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -4,7 +4,14 @@ import { AuthContext } from "../AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import ProfileDialogBox from "../components/ProfileDialogBox";
-import { Alert, Avatar, Box, DialogTitle, Snackbar } from "@mui/material";
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  DialogTitle,
+  Snackbar,
+} from "@mui/material";
 
 function ChatPage() {
   const { accessToken, setAccessToken } = React.useContext(AuthContext);
@@ -84,30 +91,30 @@ function ChatPage() {
   };
 
   return (
-    <div className="bg-slate-200 h-screen">
+    <div className="bg-slate-200 h-screen flex flex-col">
       {/* ****navbar**** */}
       <PrimarySearchAppBar
         handleLogout={logout}
         handleProfile={handleUserProfile}
         refreshPage={refreshPage}
       />
-      <Box className="flex flex-row">
+      <Box className="flex flex-row h-full">
         {/* ****Converstaions List**** */}
-        <Box className="basis-1/4 bg-slate-100 h-screen">
+        <Box className="basis-1/4 bg-slate-100 rounded-lg m-1">
           {conversations &&
             conversations.map((conversation) => {
               console.log(conversation?.member?.pic);
               return (
-                <Box className="flex items-center rounded-md border-2 active:bg-violet-700 hover:bg-blue-400  m-1 pl-5">
+                <Box className="flex items-center rounded-md border-2 active:bg-blue-400 hover:bg-blue-200  m-1 pl-5">
                   <Avatar
                     alt="Profile Picture"
                     src={`http://localhost:8000/${conversation?.member?.pic}`}
                     sx={{ width: 46, height: 46 }}
                   />
                   <Box className="flex flex-col ml-5 p-1">
-                    <text className="text-xl">
-                      {conversation?.member?.username.charAt(0).toUpperCase() +
-                        conversation?.member?.username.slice(1)}
+                    <text className="text-xl font-semibold">
+                      {conversation?.member?.username?.charAt(0).toUpperCase() +
+                        conversation?.member?.username?.slice(1)}
                     </text>
                     <text className="text-gray-500">
                       {conversation?.member?.email}
@@ -118,6 +125,100 @@ function ChatPage() {
             })}
         </Box>
         {/* ****Chat Box**** */}
+        <Box className="basis-2/4 flex flex-col p-2">
+          <Box className="h-full bg-slate-100 mb-1 rounded-lg">
+            <Box className="bg-green-300 text-gray-900 rounded-lg p-2 m-2 w-fit">
+              <text className="block font-semibold">User 1</text>
+              Hi, this is me
+            </Box>
+            <Box className="bg-blue-300 text-gray-900 rounded-lg p-2 m-2 w-fit ">
+              <text className="block font-semibold">User 2</text>
+              Hi, how are you
+            </Box>
+          </Box>
+
+          <form className="h-max">
+            <label for="chat" class="sr-only">
+              Your message
+            </label>
+            <div class="flex items-center px-3 py-2 rounded-lg bg-gray-50 ">
+              <button
+                type="button"
+                class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 "
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 18"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+                  />
+                </svg>
+                <span class="sr-only">Upload image</span>
+              </button>
+              <button
+                type="button"
+                class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 "
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z"
+                  />
+                </svg>
+                <span class="sr-only">Add emoji</span>
+              </button>
+              <textarea
+                id="chat"
+                rows="1"
+                class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border focus:border-blue-400 border-gray-300"
+                placeholder="Your message..."
+              ></textarea>
+              <button
+                type="submit"
+                class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 "
+              >
+                <svg
+                  class="w-5 h-5 rotate-90"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 20"
+                >
+                  <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                </svg>
+                <span class="sr-only">Send message</span>
+              </button>
+            </div>
+          </form>
+        </Box>
       </Box>
 
       <Snackbar open={toast} autoHideDuration={3000} onClose={handleClose}>
